f7b9231b404b6ed489f4ea5cbb3ad70f13db55c0
Jonny Saykosy <josayko@pm.me>
2022-05-27 18:25:06 +0200

    return base template from Home component

--- app/pages/index.js
+++ app/pages/index.js
@@ -5,7 +5,29 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 export default function Home() {
   return (
     <div>
-      <ConnectButton></ConnectButton>
+      <Head>
+        <title>Crypto Devs NFT</title>
+        <meta name="description" content="NFT-Collection" />
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Devs NFT!</h1>
+          <div className={styles.description}>
+            It&apos;s an NFT collection for developers in Crypto
+          </div>
+          <div className={styles.description}>/20 have been minted</div>
+          <ConnectButton />
+        </div>
+        <div>
+          <img
+            src="./cryptodevs/0.svg"
+            alt="crypto-devs-nft-logo"
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <footer className={styles.footer}>Made with &#10084; by josayko</footer>
     </div>
   )
 }
